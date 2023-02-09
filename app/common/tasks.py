a2d282db6c7f64b392e802a0ca5dbc5877af4ae2
satriafy2 <msatriadwi@gmail.com>
2023-02-09 18:40:54 +0700

    fix update task status

--- app/common/tasks.py
+++ app/common/tasks.py
@@ -1,5 +1,5 @@
 from app.db.database import db
-from app.db.model import Task
+from app.db.model import Email, Task
 from app.extension.mail import mail
 from app.extension.scheduler import scheduler
 from flask_mail import Message 
@@ -19,30 +19,33 @@ class EmailTask:
         )
         db.session.add(task)
         self.email_context.task = task
+        db.session.flush()
 
         scheduler.add_job(
             func=self.send_email,
+            args=[self.email_context.event_id],
             trigger='date',
             run_date=self.email_context.timestamp,
             id=scheduler_id,
             misfire_grace_time=5*60
         )
 
-    def send_email(self):
+    def send_email(self, email_id):
         # need to be tested
         with scheduler.app.app_context():
+            email_ctx = Email.query.filter_by(event_id=email_id).first()
             msg = Message(
-                subject=self.email_context.email_subject,
-                body=self.email_context.email_content,
+                subject=email_ctx.email_subject,
+                body=email_ctx.email_content,
                 sender='test_email_bos@mailtrap.io',
-                recipients=[self.email_context.recipient.email]
+                recipients=[email_ctx.recipient.email]
             )
 
             try:
                 mail.send(msg)
-                self.email_context.task.status = 'SENT'
+                email_ctx.task.status = 'SENT'
             except Exception as e:
                 print(e, flush=True)
-                self.email_context.task.status = 'ERROR'
+                email_ctx.task.status = 'ERROR'
 
             db.session.commit()
